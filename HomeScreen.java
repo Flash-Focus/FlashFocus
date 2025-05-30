acf8523d9c9edd58318c682ee3612e5c1e31219d
Mr-SRK <sarim.rkhan786@gmail.com>
2025-05-30 10:27:19 -0400

    Commiting all required files

--- HomeScreen.java
+++ HomeScreen.java
@@ -4,9 +4,13 @@ import javax.swing.*;
 
 public class HomeScreen {
     // Necessary J stuff
+    String apiKey;
 
     // Constructor
     public HomeScreen() {
+        
+        // DO NOT REMOVE THIS
+        apiKey = "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";
         
     }
 
